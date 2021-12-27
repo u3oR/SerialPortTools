09fd4aea912dec62f624506f785bfa12e197297b
zhiyu <93767425+woodecode@users.noreply.github.com>
2021-12-27 22:03:52 +0800

    更新了注释

--- dialog.h
+++ dialog.h
@@ -48,14 +48,52 @@
 **
 ****************************************************************************/
 
-#include <QApplication>
+#ifndef DIALOG_H
+#define DIALOG_H
 
-#include "dialog.h"
+#include <QDialog>
 
-int main(int argc, char *argv[])
+#include "masterthread.h"
+
+QT_BEGIN_NAMESPACE
+
+class QLabel;
+class QLineEdit;
+class QSpinBox;
+class QPushButton;
+class QComboBox;
+
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
 {
-    QApplication app(argc, argv);
-    Dialog dialog;
-    dialog.show();
-    return app.exec();
-}
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = nullptr);
+
+private slots:
+    void transaction();
+    void showResponse(const QString &s);
+    void processError(const QString &s);
+    void processTimeout(const QString &s);
+
+private:
+    void setControlsEnabled(bool enable);
+
+private:
+    int transactionCount;
+    QLabel *serialPortLabel;
+    QComboBox *serialPortComboBox;
+    QLabel *waitResponseLabel;
+    QSpinBox *waitResponseSpinBox;
+    QLabel *requestLabel;
+    QLineEdit *requestLineEdit;
+    QLabel *trafficLabel;
+    QLabel *statusLabel;
+    QPushButton *runButton;
+
+    MasterThread thread;
+};
+
+#endif // DIALOG_H
--- masterthread.h
+++ masterthread.h
@@ -48,14 +48,38 @@
 **
 ****************************************************************************/
 
-#include <QApplication>
+#ifndef MASTERTHREAD_H
+#define MASTERTHREAD_H
 
-#include "dialog.h"
+#include <QThread>
+#include <QMutex>
+#include <QWaitCondition>
 
-int main(int argc, char *argv[])
+//! [0]
+class MasterThread : public QThread
 {
-    QApplication app(argc, argv);
-    Dialog dialog;
-    dialog.show();
-    return app.exec();
-}
+    Q_OBJECT
+
+public:
+    explicit MasterThread(QObject *parent = nullptr);
+    ~MasterThread();
+
+    void transaction(const QString &portName, int waitTimeout, const QString &request);
+    void run() Q_DECL_OVERRIDE;
+
+signals:
+    void response(const QString &s);
+    void error(const QString &s);
+    void timeout(const QString &s);
+
+private:
+    QString portName;
+    QString request;
+    int waitTimeout;
+    QMutex mutex;
+    QWaitCondition cond;
+    bool quit;
+};
+//! [0]
+
+#endif // MASTERTHREAD_H
